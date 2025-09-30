17c206a432865e60d8138632e2cc1c54b4baa4a9
alejandro <alejandrolima1030@gmail.com>
2025-09-30 19:53:56 -0300

    Criação de rotas no routes.ts e roteamento dinamico com routeroutlet. Criação de rotas filhas com layout

--- frontend-gestao-marketplace/src/app/app.routes.ts
+++ frontend-gestao-marketplace/src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Products } from './pages/products/products';
+import { Layout } from './pages/layout/layout';
+import { NewProduct } from './pages/new-product/new-product';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: Login
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full' // necessário para redirecionar exatamente a rota vazia, se não,
+    // qualquer rota que comece com '' redireciona para /login, causando loop infinito e tela em branco
+  },
+  {
+    path: '',
+    component: Layout, //component base para carregamento das rotas filhas, ele contem
+    //o router-outlet para renderizar as rotas filhas baseadas no path
+    children: [
+      {
+        path: 'products',
+        component: Products
+      },
+      {
+        path: 'new-product',
+        component: NewProduct
+      }
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '/login',
+  }
+];
--- frontend-gestao-marketplace/src/app/app.ts
+++ frontend-gestao-marketplace/src/app/app.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-import { Login } from "./pages/login/login";
-import { Header } from "./components/header/header";
-import { Products } from "./pages/products/products";
-import { NewProduct } from "./pages/new-product/new-product";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Login, Header, Products, NewProduct],
+  imports: [RouterOutlet],
+  //Como estava de forma manual o carregamento, os componentes Login, Header, Products, NewProduct
+  // tinham que ser importados e declarados no imports
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  
+
 }
